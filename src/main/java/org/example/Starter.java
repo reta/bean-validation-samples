fadbce879800229f2c9476fdf1edbec6d7b15d21
reta <drreta@gmail.com>
2012-05-09 17:29:01 -0400

    Upgrade to Hibernate Validator 4.3.0.Final

--- src/main/java/org/example/Starter.java
+++ src/main/java/org/example/Starter.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
 public class Starter {
 	public static void main( String[] args ) {
 		new ValidatorUtil().add( Person.class, "firstName" );
 		new ValidatorUtil().find( Person.class, "firstName" );
 		new ValidatorUtil().find( Department.class, "persons" );
-		new ValidatorUtil().validate( new Person() );
+
+		try {
+			new ValidatorUtil().validate( new Person() );
+		} catch( ConstraintViolationException ex ) {
+			for( final ConstraintViolation< ? > violation: ex.getConstraintViolations() ) {
+				System.out.println( String.format(
+					"Instance: %s, property: %s",
+					violation.getRootBean(),
+					violation.getPropertyPath()
+				) );
+			}
+		}
 	}
 
 }
