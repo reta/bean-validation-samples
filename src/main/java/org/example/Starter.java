daeed8f8c1191b013bac1fa0dd1b8a1d94bf0d3b
reta <drreta@gmail.com>
2013-11-20 19:38:47 -0500

    Update to Hibernate Validator 5.0.1

--- src/main/java/org/example/Starter.java
+++ src/main/java/org/example/Starter.java
@@ -1,16 +1,28 @@
 package org.example;
 
+import java.math.BigDecimal;
+
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
 public class Starter {
-	public static void main( String[] args ) {
-		new ValidatorUtil().add( Person.class, "firstName" );
-		new ValidatorUtil().find( Person.class, "firstName" );
-		new ValidatorUtil().find( Department.class, "persons" );
+	public static void main( String[] args ) throws NoSuchMethodException, SecurityException {
+		final ValidatorUtil validatorUtil = new ValidatorUtil();
+		
+        validatorUtil.add( Person.class, "firstName" );
+		validatorUtil.find( Person.class, "firstName" );
+		validatorUtil.find( Department.class, "persons" );
 
 		try {
-			new ValidatorUtil().validate( new Person( new Department( "D") ) );
+		    final Person person = new Person( new Department( "Dept" ) );
+		    
+		    validatorUtil.validate( 
+		        Person.class.getMethod( "saveItem", Person.class, BigDecimal.class ), 
+		        new Object[] { person, new BigDecimal( 0 ) }, 
+		        person
+		    );
+		    
+			validatorUtil.validate( new Person( new Department( "D" ) ) );
 		} catch( ConstraintViolationException ex ) {
 			for( final ConstraintViolation< ? > violation: ex.getConstraintViolations() ) {
 				System.out.println( String.format(
